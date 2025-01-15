92c5d8f3312b68cb8fc1fab45b18c19de18118dc
nov-bohdan <nov.bohdan@gmail.com>
2025-01-14 19:22:59 -0600

    Removed unnecessary div

--- src/components/ChatListSection.tsx
+++ src/components/ChatListSection.tsx
@@ -13,22 +13,20 @@ export default function ChatListSection({
       <h3 className="text-xs font-semibold px-2 mb-2">{header}</h3>
       {chats.map((chat) => {
         return (
-          <div key={chat.id} className="">
-            <div className="flex flex-col gap-2">
-              <Link href={`/c/${chat.id}`}>
-                <div
-                  className="text-sm cursor-pointer hover:bg-gray-500 hover:bg-opacity-10 p-2 rounded-lg truncate"
-                  style={{
-                    WebkitMaskImage:
-                      "linear-gradient(to right, black 60%, transparent 100%)",
-                    maskImage:
-                      "linear-gradient(to right, black 60%, transparent 100%)",
-                  }}
-                >
-                  {chat.title}
-                </div>
-              </Link>
-            </div>
+          <div key={chat.id} className="flex flex-col gap-2">
+            <Link href={`/c/${chat.id}`}>
+              <div
+                className="text-sm cursor-pointer hover:bg-gray-500 hover:bg-opacity-10 p-2 rounded-lg truncate"
+                style={{
+                  WebkitMaskImage:
+                    "linear-gradient(to right, black 60%, transparent 100%)",
+                  maskImage:
+                    "linear-gradient(to right, black 60%, transparent 100%)",
+                }}
+              >
+                {chat.title}
+              </div>
+            </Link>
           </div>
         );
       })}
